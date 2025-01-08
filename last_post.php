2f0e85e0390cb00687ca24bebe08d6ab1e885407
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-01-08 10:37:19 +0000

    2025-01-05

--- last_post.php
+++ last_post.php
@@ -1,56 +1,88 @@
 <div class="card">
     <div class="card-header">
-        <h3>29.&nbsp;grudnia 2024&nbsp;r.</h3>
-        <h3>Święto Świętej Rodziny, Rok&nbsp;C</h3>
+        <h3>5.&nbsp;grudnia 2024&nbsp;r.</h3>
+        <h3>II&nbsp;NIEDZIELA Po&nbsp;Narodzeniu Pańskim, Rok&nbsp;C</h3>
     </div>
     <div class="col">
-        <p>1. We&nbsp;wtorek o&nbsp;godz.&nbsp;18.00 Msza&nbsp;św.
-        na&nbsp;zakończenie roku z&nbsp;uroczystym „Te Deum”
-        i&nbsp;błogosławieństwem Najświętszym Sakramentem.</p>
-        <p>2. W&nbsp;środę – 1&nbsp;stycznia – Uroczystość Świętej Bożej
-        Rodzicielki – Nowy Rok. Światowy Dzień Pokoju.</p>
-        <p>3. <strong>Prządek Wizyty Duszpasterskiej:</strong></p>
+        <p>1. Ks.&nbsp;Biskup Ordynariusz odwołał z&nbsp;funkcji wikariusza
+        w&nbsp;naszej parafii ks.&nbsp;Krystiana Kałużę i&nbsp;skierował
+        do&nbsp;posługi kapłańskiej w&nbsp;parafii w&nbsp;Klimontowie.
+        Dziękujemy Mu za&nbsp;posługę w&nbsp;naszej parafii i&nbsp;życzymy
+        zdrowia, i&nbsp;wszelkich łask w&nbsp;kapłańskim życiu.</p>
+        <p>2. W&nbsp;niedzielę 19&nbsp;stycznia br. będziemy gościć
+        w&nbsp;naszej parafii Reprezentacyjną Orkiestrę Służby Granicznej.
+        Wystąpi ona w&nbsp;kościele na&nbsp;Mszy&nbsp;św. na&nbsp;10.00,
+        a&nbsp;po&nbsp;Niej koncert kolęd w&nbsp;wykonaniu solistów
+        i&nbsp;towarzyszącej im&nbsp;orkiestry. Jak zauważyliście nastąpi
+        zmiana w&nbsp;godzinach Mszy Świętych niedzielnych. Tego dnia,
+        tj.&nbsp;19&nbsp;stycznia br. nie będzie Mszy&nbsp;św.
+        o&nbsp;godz.&nbsp;9.30 i&nbsp;11.15, a&nbsp;o&nbsp;wspomnianej 10.00,
+        w&nbsp;tej Mszy&nbsp;św. koncelebrowanej będą polecane Panu Bogu,
+        intencje mszalne z&nbsp;9.30 i&nbsp;11.15. Prosimy zapamiętać
+        i&nbsp;poinformować innych. Zapraszamy do&nbsp;udziału
+        we&nbsp;Mszy&nbsp;św. o&nbsp;10.00 i&nbsp;w&nbsp;koncercie.</p>
+        <p>3. W&nbsp;najbliższy poniedziałek 6&nbsp;stycznia 2025 r.
+        – Uroczystość Objawienia Pańskiego – w&nbsp;tradycji: Święto Trzech
+        Króli – prządek Mszy&nbsp;św. jak w&nbsp;niedzielę. Poświęcenie kredy
+        i&nbsp;kadzidła na&nbsp;Mszach św. Poświęconą kredą oznaczamy odrzwia
+        naszych domów i&nbsp;mieszkań C+M+B 2025. O&nbsp;godz.&nbsp;12.00
+        w&nbsp;tym dniu wyruszy od&nbsp;naszego kościoła orszak z&nbsp;jednym
+        z&nbsp;„Trzech Króli”.  Wspólnie udamy się ze&nbsp;śpiewem kolęd
+        do&nbsp;stajenki na&nbsp;rynku Starego Miasta, aby złożyć pokłon
+        Nowonarodzonemu Królowi Świata. Zakończenie uroczystości
+        o&nbsp;godz.&nbsp;13.30. Będzie gorąca herbata i&nbsp;pączek.
+        Zapraszamy wszystkich, dorosłych, młodzież i&nbsp;dzieci! Zbieramy
+        środki pielęgnacyjne, higieny i&nbsp;czystości dla Domu Pomocy
+        Społecznej.</p>
 
-        <p style="font-weight: bold; text-decoration: underline;">Poniedziałek
-        od&nbsp;godz.&nbsp;15.00</p>
-        <p>I&nbsp;Ksiądz – ul.&nbsp;Asnyka i&nbsp;Reymonta</p>
-        <p>II&nbsp;Ksiądz – ul.&nbsp;Warzywna i&nbsp;Jabłoniowa</p>
-        <p>III&nbsp;Ksiądz – ul.&nbsp;Sadownicza i&nbsp;Brzoskwiniowa</p>
-        <p>IV&nbsp;Ksiądz – Sucharzów</p>
+        <p>4. <strong>Prządek Wizyty Duszpasterskiej: od&nbsp;07
+        do&nbsp;11&nbsp;stycznia br.</strong></p>
 
         <p style="font-weight: bold; text-decoration: underline;">Wtorek
         od&nbsp;godz.&nbsp;15.00</p>
-        <p>I&nbsp;Ksiądz – II&nbsp;Ksiądz – ul.&nbsp;15-tego Sierpnia –
-        z&nbsp;końców ulicy do&nbsp;zejścia się.</p>
-        <p>III&nbsp;Ksiądz – ul.&nbsp;Grodzisko</p>
-        <p>IV&nbsp;Ksiądz – ul.&nbsp;Mikołaja Reja</p>
-        <p><strong>W&nbsp;Nowy Rok podamy dalszy porządek wizyty
-        duszpasterskiej w&nbsp;tym tygodniu.</strong></p>
+        <p>I&nbsp;Ksiądz – ul.&nbsp;Rolnicza ul.&nbsp;Ożarowska
+        od&nbsp;nr&nbsp;123 w&nbsp;kierunku Sandomierza</p>
+        <p>II&nbsp;Ksiądz – ul.&nbsp;Ożarowska od&nbsp;nr.&nbsp;5
+        do&nbsp;zejścia</p>
+        <p>III&nbsp;Ksiądz – Batalionów Chłopskich, Wiosenna
+        i&nbsp;od&nbsp;16.00 Wiśniowa</p>
 
-        <p>4. W&nbsp;przyszłą niedzielę w&nbsp;kościele, pół godziny przed
-        Mszą&nbsp;św. o&nbsp;godz.&nbsp;11.15 – adoracja Najśw. Sakramentu
-        połączona z&nbsp;modlitwą różańcową, którą poprowadzi Koło Żywego
-        Różańca z&nbsp;naszej parafii i&nbsp;zmiana tajemnic różańcowych.
-        Zapraszamy do&nbsp;udziału w&nbsp;tej modlitwie.</p>
-        <p>5. W&nbsp;przyszłą niedzielę – 5&nbsp;stycznia –
-        o&nbsp;godz.&nbsp;15.00 odbędzie się kolędowanie przy szopce
-        w&nbsp;Milczanach. Zapraszamy rodziny z&nbsp;dziećmi do&nbsp;wspólnego
-        kolędowania.</p>
-        <p>6. Sandomierski Chór Katedralny zaprasza na&nbsp;Noworoczny Koncert
-        Kolęd, który odbędzie się 1-szego&nbsp;stycznia br.
-        o&nbsp;godz.&nbsp;16.00 w&nbsp;bazylice Katedralnej
-        w&nbsp;Sandomierzu.</p>
-        <p>7. W&nbsp;tym tygodniu Pierwszy czwartek, piątek i&nbsp;pierwsza
-        sobota miesiąca. W&nbsp;Pierwszy Piątek Miesiąca nie organizujemy
-        spowiedzi, była przed świętami Bożego Narodzenia. Prosimy skorzystać
-        z&nbsp;sakramentu pokuty w&nbsp;Nowy Rok, czyli w&nbsp;środę.
-        Przypominamy, że w&nbsp;pierwszy piątek trzeba przystąpić
-        do&nbsp;komunii św. To&nbsp;jest istota nabożeństwa
-        pierwszopiątkowego.</p>
-        <p>8. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św. życzymy
-        błogosławionej niedzieli.</p>
+        <p style="font-weight: bold; text-decoration: underline;">Środa
+        od&nbsp;godz.&nbsp;15.00</p>
+        <p>I&nbsp;Ksiądz –  ul.&nbsp;Kochanowskiego od&nbsp;nr&nbsp;2</p>
+        <p>II&nbsp;Ksiądz – ul.&nbsp;Kochanowskiego od&nbsp;nr&nbsp;72
+        do&nbsp;zejścia</p>
+        <p>III&nbsp;Ksiądz – ul.&nbsp;Sucharzowska i&nbsp;Klonowa</p>
+
+        <p style="font-weight: bold; text-decoration: underline;">Czwartek
+        od&nbsp;godz.&nbsp;15.00</p>
+        <p><storng>Ul.&nbsp;Dąbrowskiego</storng></p>
+        <p>I&nbsp;Ksiądz – ul.&nbsp;Szczęśliwa i&nbsp;następnie
+        ul.&nbsp;Dąbrowskiego od&nbsp;nr.&nbsp;3</p>
+        <p>II&nbsp;Ksiądz – od&nbsp;nr.&nbsp;64 do&nbsp;zejścia
+        i&nbsp;ul.&nbsp;Młodożeńca</p>
+        <p>III&nbsp;Ksiądz – Gałczyńskiego, Frankowskiego i&nbsp;Wiejska</p>
+
+
+        <p style="font-weight: bold; text-decoration: underline;">Piątek
+        od&nbsp;godz.&nbsp;15.00 Kobierniki</p>
+        <p>I&nbsp;Ksiądz – od&nbsp;nr&nbsp;119 i&nbsp;kolejne przy drodze
+        w&nbsp;kierunku Milczan</p>
+        <p>II&nbsp;Ksiądz – od&nbsp;nr&nbsp;197 następnie 195, 194
+        do&nbsp;zejścia</p>
+        <p>III&nbsp;Ksiądz numery domów: 219, 218, 216, 212, 210,
+        a&nbsp;potem 230 i&nbsp;dalej po&nbsp;drodze do&nbsp;Sandomierza</p>
+
+        <p style="font-weight: bold; text-decoration: underline;">Sobota
+        od&nbsp;godz.&nbsp;9.00 Kobierniki</p>
+        <p>I&nbsp;Ksiądz – od&nbsp;ul.&nbsp;Polnej</p>
+        <p>II&nbsp;Ksiądz – od&nbsp;ul.&nbsp;Mickiewicza nr&nbsp;115, 116,
+        107, 106 kolejne do&nbsp;zejścia</p>
+        <p>III&nbsp;Ksiądz – ul.&nbsp;Obrońców Westerplatte i&nbsp;ul.&nbsp;Przytulna</p>
+
+        <p>5. Wszystkim uczestniczącym we Mszy św. życzymy błogosławionej niedzieli.</p>
         <p>
-            <span style="float: left;">Sandomierz, 29.&nbsp;grudnia 2024&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 5.&nbsp;grudnia 2024&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
         </p>
     </div>
--- wizyta_duszpasterska.php
+++ wizyta_duszpasterska.php
@@ -14,22 +14,47 @@
     </div>
 
     <div class="col">
-        <p style="font-weight: bold; text-decoration: underline;">30&nbsp;GRUDNIA (PONIEDZIAŁEK)</p>
+        <p style="font-weight: bold; text-decoration: underline;">7&nbsp;STYCZNIA (WTOREK)</p>
         <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
-        <p>I&nbsp;Ksiądz – ul.&nbsp;Asnyka i&nbsp;Reymonta</p>
-        <p>II&nbsp;Ksiądz – ul.&nbsp;Warzywna i&nbsp;Jabłoniowa</p>
-        <p>III&nbsp;Ksiądz – ul.&nbsp;Sadownicza i&nbsp;Brzoskwiniowa</p>
-        <p>IV&nbsp;Ksiądz – Sucharzów</p><br>
+        <p>I&nbsp;Ksiądz – ul.&nbsp;Rolnicza ul.&nbsp;Ożarowska
+        od&nbsp;nr&nbsp;123 w&nbsp;kierunku Sandomierza</p>
+        <p>II&nbsp;Ksiądz – ul.&nbsp;Ożarowska od&nbsp;nr.&nbsp;5
+        do&nbsp;zejścia</p>
+        <p>III&nbsp;Ksiądz – Batalionów Chłopskich, Wiosenna
+        i&nbsp;od&nbsp;16.00 Wiśniowa</p><br>
 
-        <p style="font-weight: bold; text-decoration: underline;">31&nbsp;GRUDNIA (WTOREK)</p>
+        <p style="font-weight: bold; text-decoration: underline;">8&nbsp;STYCZNIA (ŚRODA)</p>
         <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
-        <p>I&nbsp;Ksiądz – II&nbsp;Ksiądz – ul.&nbsp;15-tego Sierpnia –
-        z&nbsp;końców ulicy do&nbsp;zejścia się.</p>
-        <p>III&nbsp;Ksiądz – ul.&nbsp;Grodzisko</p>
-        <p>IV&nbsp;Ksiądz – ul.&nbsp;Mikołaja Reja</p><br>
+        <p>I&nbsp;Ksiądz –  ul.&nbsp;Kochanowskiego od&nbsp;nr&nbsp;2</p>
+        <p>II&nbsp;Ksiądz – ul.&nbsp;Kochanowskiego od&nbsp;nr&nbsp;72
+        do&nbsp;zejścia</p>
+        <p>III&nbsp;Ksiądz – ul.&nbsp;Sucharzowska i&nbsp;Klonowa</p><br>
 
-        <p><strong>W&nbsp;Nowy Rok podamy dalszy porządek wizyty
-        duszpasterskiej w&nbsp;tym tygodniu.</strong></p><br>
+        <p style="font-weight: bold; text-decoration: underline;">9&nbsp;STYCZNIA (CZWARTEK)</p>
+        <p><strong>Od&nbsp;godz.&nbsp;15.00:</strong></p>
+        <p><storng>Ul.&nbsp;Dąbrowskiego</storng></p>
+        <p>I&nbsp;Ksiądz – ul.&nbsp;Szczęśliwa i&nbsp;następnie
+        ul.&nbsp;Dąbrowskiego od&nbsp;nr.&nbsp;3</p>
+        <p>II&nbsp;Ksiądz – od&nbsp;nr.&nbsp;64 do&nbsp;zejścia
+        i&nbsp;ul.&nbsp;Młodożeńca</p>
+        <p>III&nbsp;Ksiądz – Gałczyńskiego, Frankowskiego i&nbsp;Wiejska</p><br>
+
+        <p style="font-weight: bold; text-decoration: underline;">10&nbsp;STYCZNIA (PIĄTEK)</p>
+        <p><strong>Od&nbsp;godz.&nbsp;15.00 Kobierniki:</strong></p>
+        <p>I&nbsp;Ksiądz – od&nbsp;nr&nbsp;119 i&nbsp;kolejne przy drodze
+        w&nbsp;kierunku Milczan</p>
+        <p>II&nbsp;Ksiądz – od&nbsp;nr&nbsp;197 następnie 195, 194
+        do&nbsp;zejścia</p>
+        <p>III&nbsp;Ksiądz numery domów: 219, 218, 216, 212, 210,
+        a&nbsp;potem 230 i&nbsp;dalej po&nbsp;drodze do&nbsp;Sandomierza</p><br>
+
+        <p style="font-weight: bold; text-decoration: underline;">11&nbsp;STYCZNIA (SOBOTA)</p>
+        <p><strong>Od&nbsp;godz.&nbsp;9.00 Kobierniki:</strong></p>
+        <p>I&nbsp;Ksiądz – od&nbsp;ul.&nbsp;Polnej</p>
+        <p>II&nbsp;Ksiądz – od&nbsp;ul.&nbsp;Mickiewicza nr&nbsp;115, 116,
+        107, 106 kolejne do&nbsp;zejścia</p>
+        <p>III&nbsp;Ksiądz – ul.&nbsp;Obrońców Westerplatte i&nbsp;ul.&nbsp;Przytulna</p><br>
+
     </div>
 </div>
 <br>
