115cfab38231ec8b4c81e4fa880fbbd327d46ff6
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-01-12 02:15:12 +0000

    2025-01-12

--- archiwum14.php
+++ archiwum14.php
@@ -19,7 +19,153 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2025 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 5.&nbsp;stycznia 2025&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 12.&nbsp;stycznia 2025&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    6.12. – 12.01.2025&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 6&nbsp;stycznia</b></td>
+                                    <td><b><span style="color: red">UROCZYSTOŚĆ OBJAWIENIA
+                                    PAŃSKIEGO</span></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Stanisława Czajkę i&nbsp;zm. z&nbsp;rodz. Czajków
+                                    i&nbsp;Gachów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Teresę i&nbsp;Józefa Janasów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ rodziców Władysławę i&nbsp;Józefa Gospodarczyków,
+                                    Władysława Gospodarczyka, Dymitra i&nbsp;Mariannę
+                                    Chlebników</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Jana, Anielę Majewskich, Stefana i&nbsp;Zofię
+                                    Smolińskich</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
+                                    Kacpra, Wojciecha i&nbsp;Barbary</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 7&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + ks. Jana Tosia<br>
+                                    2. + Marka Frańczaka<br>
+                                    3. + Joannę Branię</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Wiesława Napieracza, Halinę i&nbsp;Wacława
+                                    Bryłów</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 8&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Marka Frańczaka<br>
+                                    2. + Kazimierza Karę<br>
+                                    3. + Jana Pazdro</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Mariana Biało</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 9&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Marka Frańczaka<br>
+                                    2. + Zofię Wałcerz<br>
+                                    3. + Kazimierza Karę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Mariana Biało</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 10&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Marka Frańczaka<br>
+                                    2. + Kazimierza Karę<br>
+                                    3. + Jana Pazdro</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>O&nbsp;łaskę doświadczenia Bożej miłości dla męża
+                                    Piotra i&nbsp;ojca Tadeusza</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 11&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Teodozję rocz. śm., Zdzisława, zm z&nbsp;rodz.,
+                                        dusze w&nbsp;czyśćcu cierpiące<br>
+                                    2. + Jana Pazdro<br>
+                                    3. + Zuzannę Chrząstowską</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ ks.&nbsp;Jana Tosia</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 12&nbsp;stycznia</b></td>
+                                    <td><b>
+                                        <span style="color: red">ŚWIĘTO CHRZTU PAŃSKIEGO</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Michała Dudę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Kazimierza, Mariannę Kieszkowskich, zm. z&nbsp;rodz.
+                                    Kieszkowskich i&nbsp;Nizielskich</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ Jadwigę, Czesława, Dariusza Skroków, Marię, Józefa
+                                    Dziubów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Janinę z&nbsp;okazji urodzin</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,134 +1,118 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            6.12. – 12.01.2025&nbsp;r.
+            13.12. – 19.01.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 6&nbsp;stycznia</b></td>
-            <td><b><span style="color: red">UROCZYSTOŚĆ OBJAWIENIA
-            PAŃSKIEGO</span></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">7.30</td>
-            <td>+ Stanisława Czajkę i&nbsp;zm. z&nbsp;rodz. Czajków
-            i&nbsp;Gachów</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">9.30</td>
-            <td>+ Teresę i&nbsp;Józefa Janasów</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">11.15</td>
-            <td>+ rodziców Władysławę i&nbsp;Józefa Gospodarczyków,
-            Władysława Gospodarczyka, Dymitra i&nbsp;Mariannę
-            Chlebników</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">12.30</td>
-            <td>+ Jana, Anielę Majewskich, Stefana i&nbsp;Zofię
-            Smolińskich</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
-            Kacpra, Wojciecha i&nbsp;Barbary</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>WTOREK, 7&nbsp;stycznia</b></td>
+            <td><b>PONIEDZIAŁEK, 13&nbsp;stycznia</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + ks. Jana Tosia<br>
-            2. + Marka Frańczaka<br>
-            3. + Joannę Branię</td>
+            <td>1. W&nbsp;int. Asi z&nbsp;racji urodzin
+                o&nbsp;potrzebne łaski<br>
+            2. + Jana Pazdro<br>
+            3. + Stanisławę Kozioł</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Wiesława Napieracza, Halinę i&nbsp;Wacława
-            Bryłów</td>
+            <td>1. + Marię, Władysława, Czesława Barańskich
+            i&nbsp;Andrzeja Krawczyka</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>ŚRODA, 8&nbsp;stycznia</b></td>
+            <td><b>WTOREK, 14&nbsp;stycznia</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Marka Frańczaka<br>
-            2. + Kazimierza Karę<br>
-            3. + Jana Pazdro</td>
+            <td>1. + Jana Pazdro<br>
+            2. + Jerzego Janiszewskiego<br>
+            3. + Stanisławę Kozioł</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Mariana Biało</td>
+            <td>1. KWC</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 9&nbsp;stycznia</b></td>
+            <td><b>ŚRODA, 15&nbsp;stycznia</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Marka Frańczaka<br>
-            2. + Zofię Wałcerz<br>
-            3. + Kazimierza Karę</td>
+            <td>1. + Zdzisława Bernasia 5&nbsp;rocz. śm.<br>
+            2. + Jana Pazdro<br>
+            3. + Annę Płaza<br>
+            4. + Zdzisława Bernasia 5&nbsp;rocz. śm.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Mariana Biało</td>
+            <td>1. + Stanisława Dobrocha</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 10&nbsp;stycznia</b></td>
+            <td><b>CZWARTEK, 16&nbsp;stycznia</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Marka Frańczaka<br>
-            2. + Kazimierza Karę<br>
-            3. + Jana Pazdro</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>O&nbsp;łaskę doświadczenia Bożej miłości dla męża
-            Piotra i&nbsp;ojca Tadeusza</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>SOBOTA, 11&nbsp;stycznia</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Teodozję rocz. śm., Zdzisława, zm z&nbsp;rodz.,
-                dusze w&nbsp;czyśćcu cierpiące<br>
+            <td>1. + Kazimierza Karę<br>
             2. + Jana Pazdro<br>
             3. + Zuzannę Chrząstowską</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ ks.&nbsp;Jana Tosia</td>
+            <td>1. 18&nbsp;rocz. powstania Legionu Maryi</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 12&nbsp;stycznia</b></td>
+            <td><b>PIĄTEK, 17&nbsp;stycznia</b></td>
+            <td><b>Wspomnienie św.&nbsp;Antoniego Pustelnika</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Joannę Branię<br>
+            2. + Jana Pazdro<br>
+            3. + Stanisławę Kozioł</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Zofię Wałcerz</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>SOBOTA, 18&nbsp;stycznia</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Jerzego Janiszewskiego<br>
+            2. + Zuzannę Chrząstowską</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. O&nbsp;Boże błog. dla Mai Szwagierczak
+                10&nbsp;rocz. urodzin<br>
+            2. + Jana Skorubskiego 31&nbsp;rocz. śm.</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>NIEDZIELA, 19&nbsp;stycznia</b></td>
             <td><b>
-                <span style="color: red">ŚWIĘTO CHRZTU PAŃSKIEGO</span>
+                <span style="color: red">II&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Michała Dudę</td>
+            <td>+ Edwarda Nowińskiego, Stefana i&nbsp;Stefanię
+            Szpakowskich</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
-            <td class="center">9.30</td>
-            <td>+ Kazimierza, Mariannę Kieszkowskich, zm. z&nbsp;rodz.
-            Kieszkowskich i&nbsp;Nizielskich</td>
+            <td class="center">10.00</td>
+            <td>+ Sylwestra Gajka 10&nbsp;rocz. śm.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
-            <td class="center">11.15</td>
-            <td>+ Jadwigę, Czesława, Dariusza Skroków, Marię, Józefa
-            Dziubów</td>
+            <td class="center">10.00</td>
+            <td>DK</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
@@ -136,10 +120,26 @@
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Janinę z&nbsp;okazji urodzin</td>
+            <td>+ Genowefę i&nbsp;Stanisława Rusaków</td>
         </tr>
     </tbody>
 </table>
     </div>
 </div>
 <br>
+<div class="card">
+    <div class="card-header">
+        UWAGA
+    </div>
+    <div class="col">
+    <p style="text-decoration: underline; font-weight: bold;">UWAGA!</p>
+    <p><strong>Przypominamy, że&nbsp;w&nbsp;niedzielę 19&nbsp;stycznia
+    nie ma&nbsp;Mszy Świętej o&nbsp;godz.&nbsp;9.30 i&nbsp;11.15 –
+    jest jedna Msza Święta o&nbsp;godz.&nbsp;10.00,
+    a&nbsp;po&nbsp;niej koncert Orkiestry Reprezentacyjnej Straży
+    Granicznej</strong></p>
+    <p><strong>Intencje z&nbsp;godz.&nbsp;9.30 i&nbsp;11.15 będą
+    polecane Panu Bogu na&nbsp;Mszy&nbsp;Św.
+    o&nbsp;godz.&nbsp;10.00.</strong></p>
+    </div>
+</div>
