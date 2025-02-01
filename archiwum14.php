4647b427495104393d1a6f10681c827a3d868963
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-02-01 13:04:24 +0000

    2025-02-02

--- archiwum14.php
+++ archiwum14.php
@@ -19,7 +19,134 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2025 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 26.&nbsp;stycznia 2025&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 2.&nbsp;lutego 2025&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    27.01. – 02.02.2025&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 27&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Szczepana w&nbsp;rocz. śm., Jadwigę, zm.
+                                    z&nbsp;rodz. i&nbsp;dusze w&nbsp;czyśću cierpiące</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jerzego Janiszewskiego<br>
+                                    2. + Eugeniusza Wałcerza</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 28&nbsp;stycznia</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Tomasza z&nbsp;Akwinu</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Zuzannę Chrząstowską</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Stanisławę i&nbsp;Genowefę, zm. z&nbsp;rodz.<br>
+                                    2. + Stanisławę Kozioł</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 29&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Stanisławę Kozioł</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
+                                        Krystiana – rocz. urodzin<br>
+                                    2. + Aleksandrę Durę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 30&nbsp;stycznia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Joannę Branię</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Aleksandrę Durę<br>
+                                    2. + Stanisławę Kozioł</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 31&nbsp;stycznia</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Jana Bosko</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Stanisławę Kozioł</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Mariana Miękinę – 1&nbsp;rocz. śm., Zofię Miękinę<br>
+                                    2. + Marka Frańczaka</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 1&nbsp;lutego</b></td>
+                                    <td><b>I&nbsp;sobota miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Jerzego Janiszewskiego<br>
+                                    2. + Franciszka Muchę, Władysławę, Władysława Grudniów,
+                                    Kazimierę Bryła</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. LM<br>
+                                    2. + Jana, Stanisławę, Witolda, Mariana, Stanisława,
+                                    Krystynę, Mariana, Janinę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 2&nbsp;lutego</b></td>
+                                    <td><b>
+                                        <span style="color: red">IV&nbsp;Niedziela Zwykła
+                                        – Święto Ofiarowania Pańskiego</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Annę i&nbsp;Stanisława Janiszewskich</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Wojciecha Dobrocha – rocz. śm., jego rodziców Annę
+                                    i&nbsp;Czesława, brata Stanisława, Wincentego Łukaszka,
+                                    Mariannę Ferenc</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>KŻR – Teodora Kwiecień</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Anielę, Stefana i&nbsp;Jana Jędrzejewskich</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,123 +1,134 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            27.01. – 02.02.2025&nbsp;r.
+            03.02. – 09.02.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 27&nbsp;stycznia</b></td>
+            <td><b>PONIEDZIAŁEK, 3&nbsp;lutego</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Szczepana w&nbsp;rocz. śm., Jadwigę, zm.
-            z&nbsp;rodz. i&nbsp;dusze w&nbsp;czyśću cierpiące</td>
+            <td>1. + Zuzannę Chrząstowską<br>
+            2. + Annę Płazę</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Marka Frańczaka<br>
+            2. + Joannę Branię</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>WTOREK, 4&nbsp;lutego</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Stanisławę Kozioł<br>
+            2. + Annę Płazę</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Eugeniusza Wałcerza<br>
+            2. + Martę Łabęcką</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>ŚRODA, 5&nbsp;lutego</b></td>
+            <td><b>Wspomnienie Św.&nbsp;Agaty</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Stanisławę Kozioł<br>
+            2. + Annę Płazę, Krzysztofa, Janinę i&nbsp;Czesława
+            Płazów</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Martę Łabęcką<br>
+            2. + Marią Homa 2&nbsp;rocz. śm., Jagwigę Miara</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>CZWARTEK, 6&nbsp;lutego</b></td>
+            <td><b>Wspomnienie Świętych męczenników Pawła Miki
+                i&nbsp;towarzyszy<br>
+            I&nbsp;Czwartek miesiąca</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                kapłańskie<br>
+            2. + Annę Płazę</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Irenę rocz. śm., Tadeusza, Krzysztofa, Mirosława
+                Kołaczów<br>
+            2. + Zofię Wałcerz</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>PIĄTEK, 7&nbsp;lutego</b></td>
+            <td><b>I&nbsp;Piątek miesiąca</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. O&nbsp;potrzebne łaski dla dzieci
+                i&nbsp;chrześniaków<br>
+            2. + Martę Łabęcką</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>1. + Jerzego Janiszewskiego<br>
-            2. + Eugeniusza Wałcerza</td>
+            2. + Zuzannę Chrząstowską</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 28&nbsp;stycznia</b></td>
-            <td><b>Wspomnienie św.&nbsp;Tomasza z&nbsp;Akwinu</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Zuzannę Chrząstowską</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Stanisławę i&nbsp;Genowefę, zm. z&nbsp;rodz.<br>
-            2. + Stanisławę Kozioł</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>ŚRODA, 29&nbsp;stycznia</b></td>
+            <td><b>SOBOTA, 8&nbsp;lutego</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Stanisławę Kozioł</td>
+            <td>1. + Stanisławę rocz. śm., Mariana Mądrych, zm.
+                z&nbsp;rodz., dusze w&nbsp;czyśćcu cierpiące<br>
+            2. + Józefa Kicińskiego</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
-                Krystiana – rocz. urodzin<br>
-            2. + Aleksandrę Durę</td>
+            <td>1. + Waclawa, Anastazję, Walentego<br>
+            2. + Martę Łabęcką</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 30&nbsp;stycznia</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Joannę Branię</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Aleksandrę Durę<br>
-            2. + Stanisławę Kozioł</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 31&nbsp;stycznia</b></td>
-            <td><b>Wspomnienie św.&nbsp;Jana Bosko</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Stanisławę Kozioł</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Mariana Miękinę – 1&nbsp;rocz. śm., Zofię Miękinę<br>
-            2. + Marka Frańczaka</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>SOBOTA, 1&nbsp;lutego</b></td>
-            <td><b>I&nbsp;sobota miesiąca</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Jerzego Janiszewskiego<br>
-            2. + Franciszka Muchę, Władysławę, Władysława Grudniów,
-            Kazimierę Bryła</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. LM<br>
-            2. + Jana, Stanisławę, Witolda, Mariana, Stanisława,
-            Krystynę, Mariana, Janinę</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 2&nbsp;lutego</b></td>
+            <td><b>NIEDZIELA, 9&nbsp;lutego</b></td>
             <td><b>
-                <span style="color: red">IV&nbsp;Niedziela Zwykła
-                – Święto Ofiarowania Pańskiego</span>
+                <span style="color: red">V&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Annę i&nbsp;Stanisława Janiszewskich</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">9.30</td>
-            <td>+ Wojciecha Dobrocha – rocz. śm., jego rodziców Annę
-            i&nbsp;Czesława, brata Stanisława, Wincentego Łukaszka,
-            Mariannę Ferenc</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">11.15</td>
-            <td>KŻR – Teodora Kwiecień</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">12.30</td>
             <td>Za&nbsp;parafian</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
+            <td class="center">9.30</td>
+            <td>1. + Jerzego Polaka rocz. śm., Mariannę, Janinę
+                i&nbsp;Mariana Polaków<br>
+            2. + Wojciecha Granata</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">11.15</td>
+            <td>+ Janinę, Wacława Orłowskich, Stefana Szpakowskiego
+            i&nbsp;Mariannę Dyl</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">12.30</td>
+            <td>O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
+            Madzi – 10&nbsp;rocz. urodzin i&nbsp;Maciusia –
+            8&nbsp;rocz. urodzin</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Anielę, Stefana i&nbsp;Jana Jędrzejewskich</td>
+            <td>+ Kazimierza 22&nbsp;rocz. śm., Stefana, Jana,
+            Stanisława, Jerzego Słodowników</td>
         </tr>
     </tbody>
 </table>
