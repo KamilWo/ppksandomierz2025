207d465383af0c358f221e7831121d911cfdb6d3
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-03-01 13:02:25 +0000

    2025-03-02

--- last_intentions.php
+++ last_intentions.php
@@ -1,124 +1,142 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            24.02. – 2.03.2025&nbsp;r.
+            3.03. – 9.03.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 24&nbsp;lutego</b></td>
+            <td><b>PONIEDZIAŁEK, 3&nbsp;marca</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Tadeusza Rzeszowiaka<br>
-            2. + Mariana Strzębskiego</td>
+            <td>1. + Mariana rocz. śm., Stanisławę, zm. z&nbsp;rodz.,
+                dusze w&nbsp;czyśćcu cierpiące<br>
+            2. + Wojciecha Warzochę</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Zuzannę Chrząstowską<br>
-            2. + Józefa Kicińskiego</td>
+            <td>1. + Zofię Miękinę 16&nbsp;rocz. śm., Mariana
+                Miękinę<br>
+            2. + Bolesława Blezienia – greg.</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 25&nbsp;lutego</b></td>
-            <td><b></b></td>
+            <td><b>WTOREK, 4&nbsp;marca</b></td>
+            <td><b>Święto św.&nbsp;Kazimierza, Królewicz</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Joannę Branię<br>
+            <td>1. W&nbsp;int. Ani rocz. urodzin.<br>
+            2. + Bolesława Blezienia – greg.</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Kazimierza Kaczmarskiego, Małgorzatę Walczyna<br>
+            2. + Kazimierę Czajkę i&nbsp;Kazimierza Gacha, zm.
+            z&nbsp;rodz. Czajków i&nbsp;Gachów</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>ŚRODA, 5&nbsp;marca</b></td>
+            <td><b>Środa popielcowa – rozpoczęcie Wielkiego
+            Postu</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>+ Joannę Branię</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">9.00</td>
+            <td>+ Bolesława Blezienia – greg.</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Józefę i&nbsp;Stanisława Patynów, zm.
+                z&nbsp;rodz. Reginę i&nbsp;Henryka Nasiłowskich<br>
             2. + Jana Mikulskiego</td>
         </tr>
+        <tr class="actualnosci_day">
+            <td><b>CZWARTEK, 6&nbsp;marca</b></td>
+            <td><b>I&nbsp;Czwartek miesiąca</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                kapłańskie<br>
+            2. + Józefa Kicińskiego</td>
+        </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Martę Łabęcką<br>
-            2. + Mariana Strzębskiego</td>
+            <td>1. + Bolesława Blezienia – greg.<br>
+            2. + Jana Mikuskiego</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>ŚRODA, 26&nbsp;lutego</b></td>
+            <td><b>PIĄTEK, 7&nbsp;marca</b></td>
+            <td><b>I&nbsp;Piątek miesiąca</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. O&nbsp;potrzebne łaski dla dzieci
+                i&nbsp;chrześniaków<br>
+            2. + Bolesława Blezienia – greg.</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">17.15</td>
+            <td><b>Droga krzyżowa</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Zofię Wałcerz<br>
+            2. + Ryszarda Kołeczka</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>SOBOTA, 8&nbsp;marca</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Józefa Kicińskiego<br>
-            2. O&nbsp;zdrowie dla Anastazji, Denisa
-            i&nbsp;Wiktorii</td>
+            <td>1. O&nbsp;łaskę dobrej śmierci<br>
+            2. + Bolesława Blezienia – greg.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Wojciecha Warzochę<br>
-            2. + Mariana Strzębskiego</td>
+            <td>1. + Stanisława Klimkiewicza 1&nbsp;rocz. śm.<br>
+            2. + Władysława, Zofię Szczudło, Mariana Czerepaka, Jana
+            Kędzierskiego</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 27&nbsp;lutego</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Józefa Kicińskiego<br>
-            2. + Emilię i&nbsp;Edwarda Paluchów</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Wojciecha Warzochę<br>
-            2. + Mariana Patynę, Józefę Palińską</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 28&nbsp;lutego</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Marię Woś rocz. śm., Stefana, Franciszka
-                Kowalskich, Wiesławę Wałcerz<br>
-            2. + Waldemara Pałacha</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Mateusza Osmólskiego</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>SOBOTA, 1&nbsp;marca</b></td>
-            <td><b>I&nbsp;sobota miesiąca</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Joannę Branię<br>
-            2. + Bolesława Blezienia – greg.&nbsp;1</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. LM<br>
-            2. + Jana, Stanisławę, Witolda, Mariana, Stanisława,
-            Krystynę, Mariana, Janinę</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 2&nbsp;marca</b></td>
+            <td><b>NIEDZIELA, 9&nbsp;marca</b></td>
             <td><b>
-                <span style="color: red">VIII&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">I&nbsp;Niedziela Wielkiego
+                Postu</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Bogusławę i&nbsp;Józefa Cukrowskich</td>
+            <td>+ Bolesława Blezienia – greg.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>1. + Władysławę i&nbsp;Edwarda Sowów<br>
-            2. + Bolesława Blezienia – greg.&nbsp;2</td>
+            <td>+ Kazimierza, Mariannę Kieszkowskich, zm. z&nbsp;rodz.
+            Kieszkowskich i&nbsp;Nizielskich</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>K.Ż.R. – Ewa Łukawska</td>
+            <td>+ Wacława Dragana</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>Za&nbsp;parafian</td>
+            <td>+ Martę Łabęcką</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">17.15</td>
+            <td><b>Gorzkie Żale</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Helenę rocz. śm., Mariana Przyłuckich</td>
+            <td>Za&nbsp;parafian</td>
         </tr>
     </tbody>
 </table>
