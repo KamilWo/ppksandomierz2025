0a9af4b54086194372a9d8b5d531d409e96a5335
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-03-30 15:21:58 +0100

    2025-03-30

--- archiwum14.php
+++ archiwum14.php
@@ -19,7 +19,160 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2025 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 23.&nbsp;marca 2025&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 30.&nbsp;marca 2025&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    24.03. – 30.03.2025&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 24&nbsp;marca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Bolesława Blezienia – greg.<br>
+                                    2. + Jana Makulskiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Annę i&nbsp;Stanisława Stępniów<br>
+                                    2. + Józefa Kicińskiego</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 25&nbsp;marca</b></td>
+                                    <td><b>Uroczystość Zwiastowania Pańskiego – Dzień
+                                    Świętości Życia</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Joannę Branię<br>
+                                    2. + Bolesława Blezienia – greg.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Mariannę, Tadeusza, Marka, Wiesława Stefaniaków<br>
+                                    2. + Waldemara Pałachę</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 26&nbsp;marca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Jana Radzimowskiego<br>
+                                    2. + Bolesława Blezienia – greg.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jana Mikulskiego<br>
+                                    2. + Tadeusza i&nbsp;Zbigniewa – rocz. śm.</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 27&nbsp;marca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Joannę Branię<br>
+                                    2. + Wacława Czerwca</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Bolesława Blezienia – greg.<br>
+                                    2. + Wiesława Zwolskiego 8&nbsp;rocz. śm.</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 28&nbsp;marca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Bolesława Blezienia – greg.<br>
+                                    2. + Wacława Czerwca</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">16.30</td>
+                                    <td><b>Droga Krzyżowa dla dzieci</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.15</td>
+                                    <td><b>Droga krzyżowa</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jana, Józefę Ochmeraków, Agnieszkę Terlecką<br>
+                                    2. + Krystynę, Witolda Szwedów, Janinę, Czesława
+                                        Klimontów, zm. z&nbsp;rodz. Klimontów</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 29&nbsp;marca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Joannę Branię<br>
+                                    2. + Mariannę, Tadeusza, Marka Niemczyków</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jana Mikulskiego<br>
+                                    2. + Bolesława Blezienia – greg.</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 30&nbsp;marca</b></td>
+                                    <td><b>
+                                        <span style="color: red">IV&nbsp;Niedziela Wielkiego
+                                        Postu</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Mieczysława rocz. śm., Barbarę Bryłów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>1. + Stanisława, Janinę Zimnickich<br>
+                                    2. + Bolesława Blezienia – greg.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ Martę Łabęcką</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.15</td>
+                                    <td><b>Gorzkie Żale</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ z&nbsp;rodz. Turkotów i&nbsp;Chmielów</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,149 +1,140 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            24.03. – 30.03.2025&nbsp;r.
+            31.03. – 6.04.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 24&nbsp;marca</b></td>
+            <td><b>PONIEDZIAŁEK, 31&nbsp;marca</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Bolesława Blezienia – greg.<br>
-            2. + Jana Makulskiego</td>
+            <td>1. + Waldemara Pałachę<br>
+            2. + Wacława Czerwca</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Annę i&nbsp;Stanisława Stępniów<br>
+            <td>1. + Bogusława Żyłę w&nbsp;9&nbsp;r.&nbsp;śm.<br>
             2. + Józefa Kicińskiego</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 25&nbsp;marca</b></td>
-            <td><b>Uroczystość Zwiastowania Pańskiego – Dzień
-            Świętości Życia</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Joannę Branię<br>
-            2. + Bolesława Blezienia – greg.</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Mariannę, Tadeusza, Marka, Wiesława Stefaniaków<br>
-            2. + Waldemara Pałachę</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>ŚRODA, 26&nbsp;marca</b></td>
+            <td><b>WTOREK, 1&nbsp;kwietnia</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Jana Radzimowskiego<br>
-            2. + Bolesława Blezienia – greg.</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Jana Mikulskiego<br>
-            2. + Tadeusza i&nbsp;Zbigniewa – rocz. śm.</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 27&nbsp;marca</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Joannę Branię<br>
+            <td>1. + Joannę Brania<br>
             2. + Wacława Czerwca</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Bolesława Blezienia – greg.<br>
-            2. + Wiesława Zwolskiego 8&nbsp;rocz. śm.</td>
+            <td>1. + Jana, Stanisławę, Witolda, Mariana, Stanisława,
+                Krystynę, Mariana, Janinę<br>
+            2. + Józefa Kicińskiego</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 28&nbsp;marca</b></td>
+            <td><b>ŚRODA, 2&nbsp;kwietnia</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Bolesława Blezienia – greg.<br>
-            2. + Wacława Czerwca</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">16.30</td>
-            <td><b>Droga Krzyżowa dla dzieci</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">17.15</td>
-            <td><b>Droga krzyżowa</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Jana, Józefę Ochmeraków, Agnieszkę Terlecką<br>
-            2. + Krystynę, Witolda Szwedów, Janinę, Czesława
-                Klimontów, zm. z&nbsp;rodz. Klimontów</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>SOBOTA, 29&nbsp;marca</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Joannę Branię<br>
-            2. + Mariannę, Tadeusza, Marka Niemczyków</td>
+            <td>1. + Tadeusza Krupę<br>
+            2. + Krystynę Gach</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>1. + Jana Mikulskiego<br>
-            2. + Bolesława Blezienia – greg.</td>
+            2. + zm. z&nbsp;rodziny Erkolani</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 30&nbsp;marca</b></td>
+            <td><b>CZWARTEK, 3&nbsp;kwietnia</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania<br>
+            2. + Stanisławę Kozioł</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Ryszarda Chrząstowskiego<br>
+            2. + Tadeusza Rzeszowiaka</td>
+        </tr>
+
+
+        <tr class="actualnosci_day">
+            <td><b>PIĄTEK, 4&nbsp;kwietnia</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Szczepana w&nbsp;r. śm. Emilię Dudek, Zdzisława,
+                Teodozję zm. z&nbsp;rodz., dusze w&nbsp;czyśćcu
+                cierpiące<br>
+            2. + Krystynę Gach</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Zdzisława Kulę, Tadeusza Trześniewskiego<br>
+            2. + Jana Mikulskiego</td>
+        </tr>
+
+
+        <tr class="actualnosci_day">
+            <td><b>SOBOTA, 5&nbsp;kwietnia</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. O&nbsp;potrzebne łaski dla dzieci
+                i&nbsp;chrześniaków<br>
+            2. + Krystynę Gach</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. W&nbsp;int. Legionu Maryi<br>
+            2. Mieczysława Adamczyka w&nbsp;3&nbsp;r. śm., Agnieszkę,
+                Józefa Adamczyk</td>
+        </tr>
+
+
+        <tr class="actualnosci_day">
+            <td><b>NIEDZIELA, 6&nbsp;kwietnia</b></td>
             <td><b>
-                <span style="color: red">IV&nbsp;Niedziela Wielkiego
+                <span style="color: red">V&nbsp;Niedziela Wielkiego
                 Postu</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Mieczysława rocz. śm., Barbarę Bryłów</td>
+            <td>+ Mieczysława Adamczyka (3R)</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>1. + Stanisława, Janinę Zimnickich<br>
-            2. + Bolesława Blezienia – greg.</td>
+            <td>+ Ireneusza Cichosza, Wojciecha Kaczmarka</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>+ Martę Łabęcką</td>
+            <td>W&nbsp;intencji Koła Żywego Różańca od&nbsp;Róży
+                Różańcowej z&nbsp;Milczan</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
             <td>Za&nbsp;parafian</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
-            <td class="center">17.15</td>
-            <td><b>Gorzkie Żale</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ z&nbsp;rodz. Turkotów i&nbsp;Chmielów</td>
+            <td>+ Reginę, Edwarda Stępniów</td>
         </tr>
     </tbody>
 </table>
