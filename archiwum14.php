f254d7471b0786ff624678fe1378f256ce677626
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-05-04 18:33:57 +0100

    2025-05-04

--- archiwum14.php
+++ archiwum14.php
@@ -19,7 +19,158 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2025 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 27.&nbsp;kwietnia 2025&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 4.&nbsp;maja 2025&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    28.04. – 4.05.2025&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 28&nbsp;kwietnia</b></td>
+                                    <td><b>Uroczystość św.&nbsp;Wojciecha</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Andrzeja Wojnę – greg.<br>
+                                    2. + Marię i&nbsp;Czesława Witków</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Tadeusza Krupę<br>
+                                    2. + Mieczysława Kucwaja</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 29&nbsp;kwietnia</b></td>
+                                    <td><b>Święto św.&nbsp;Katarzyny ze&nbsp;Sieny</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Wiesława Wilka<br>
+                                    2. + Jana 10&nbsp;rocz. śm., i&nbsp;jego rodziców</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Stanisławę Bednarską<br>
+                                    2. + Andrzeja Wojnę – greg.</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 30&nbsp;kwietnia</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Andrzeja Wojnę – greg.<br>
+                                    2. + Janinę Zięba</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Mariana Biało i&nbsp;zm. z&nbsp;rodz. Białów
+                                        i&nbsp;Kosmalów<br>
+                                    2. + Tadeusza Krupę</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 1&nbsp;maja</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Andrzeja Wojnę – greg.<br>
+                                    2. + Leszka Kondrata</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jana, Józefę, Leona Gołębowskich, Tadeusza
+                                        Wójcika<br>
+                                    2. + Jana, Stanisławę, Witolda, Mariana, Stanisława,
+                                        Krystynę, Mariana i&nbsp;Janine</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 2&nbsp;maja</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Atanazego</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                                        kapłańskie<br>
+                                    2. + Leszka Kondrata</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Andrzeja Wojnę – greg.<br>
+                                    2. Dzięk.-błag. o&nbsp;Boże błog., dla dzieci
+                                        i&nbsp;wnuczki</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 3&nbsp;maja</b></td>
+                                    <td><b>Uroczystość NMP, Królowej Polski</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Andrzeja Wojnę – greg.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Lucjana Sałka – rocz. śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. LM<br>
+                                    2. + Mieczysława i&nbsp;Zofię Kucwajów</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 4&nbsp;maja</b></td>
+                                    <td><b>
+                                        <span style="color: red">III&nbsp;Niedziela
+                                        Wielkanocna</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Ks. Bpa Piotra</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Zofię i&nbsp;Władysława Chrząstowskich</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>KZR – Róża Różańcowa z&nbsp;Milczan</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>Stanisławę Stefańską – 90&nbsp;rocz. urodzin –
+                                    o&nbsp;Boże błog., potrzebne łaski, opiekę MB</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Andrzeja Wojnę – greg.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,147 +1,149 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            28.04. – 4.05.2025&nbsp;r.
+            4.05. – 11.05.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 28&nbsp;kwietnia</b></td>
-            <td><b>Uroczystość św.&nbsp;Wojciecha</b></td>
+            <td><b>PONIEDZIAŁEK, 5&nbsp;maja</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>1. + Andrzeja Wojnę – greg.<br>
-            2. + Marię i&nbsp;Czesława Witków</td>
+            2. + Leszka Kondrata</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Tadeusza Krupę<br>
-            2. + Mieczysława Kucwaja</td>
+            <td>1. + z&nbsp;rodz. Kicińskich i&nbsp;Kupków<br>
+            2. + Otolię i&nbsp;Czesława Ciachów, Małgorzatę
+                Krawczyk</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 29&nbsp;kwietnia</b></td>
-            <td><b>Święto św.&nbsp;Katarzyny ze&nbsp;Sieny</b></td>
+            <td><b>WTOREK, 6&nbsp;maja</b></td>
+            <td><b>Święto Św.&nbsp;Apostosłów Filipa
+            i&nbsp;Jakuba</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Wiesława Wilka<br>
-            2. + Jana 10&nbsp;rocz. śm., i&nbsp;jego rodziców</td>
+            <td>1. + Andrzeja Wojnę – greg.<br>
+            2. + Leszka Kondrata</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Stanisławę Bednarską<br>
+            <td>1. + Weronikę, Salomeę, Rafała Pawelców<br>
+            2. + Stanisława Oszczudłowskiego, Zofię Gospodarczyk,
+                Stanisława Dumę</td>
+        </tr>
+
+
+        <tr class="actualnosci_day">
+            <td><b>ŚRODA, 7&nbsp;maja</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. O&nbsp;potrzebne łaski dla dzieci
+                i&nbsp;chrześniaków<br>
+            2. + Jana Chmurę</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Stanisława, Leokadię Majcherów, Stanisława,
+                Jadwigę Karasińskich, Zofię Woźniak<br>
             2. + Andrzeja Wojnę – greg.</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>ŚRODA, 30&nbsp;kwietnia</b></td>
+            <td><b>CZWARTEK, 8&nbsp;maja</b></td>
+            <td><b>Uroczystość św.&nbsp;Stanisława Bp
+            i&nbsp;Męczennika</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Andrzeja Wojnę – greg.<br>
+            2. + Stanisławę i&nbsp;Władysława Bokwów</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. O&nbsp;Boże błog. dla Stanisława i&nbsp;Stanisławy
+                Niezgodów w&nbsp;dniu imienin<br>
+            2. + wiesława Napieracza, Halinę i&nbsp;Wacława Bryłów</td>
+        </tr>&nbsp;
+
+
+        <tr class="actualnosci_day">
+            <td><b>PIĄTEK, 9&nbsp;maja</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>1. + Andrzeja Wojnę – greg.<br>
-            2. + Janinę Zięba</td>
+            2. + Eugenię, Marię, Agnieszkę, Teresę, Stefana,
+                Stanisława, Henryka Koronów, Annę i&nbsp;Stanisława;
+                Justynę, Teresę, Henryka, Jana, Leszka Sapielaków</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Mariana Biało i&nbsp;zm. z&nbsp;rodz. Białów
-                i&nbsp;Kosmalów<br>
-            2. + Tadeusza Krupę</td>
+            <td>1. O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
+                    Karoliny i&nbsp;Piotra – 13&nbsp;rocz. ślubu<br>
+            2. + Stanisławę i&nbsp;Stanisława z&nbsp;racji imienin</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 1&nbsp;maja</b></td>
+            <td><b>SOBOTA, 10&nbsp;maja</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Andrzeja Wojnę – greg.<br>
-            2. + Leszka Kondrata</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Jana, Józefę, Leona Gołębowskich, Tadeusza
-                Wójcika<br>
-            2. + Jana, Stanisławę, Witolda, Mariana, Stanisława,
-                Krystynę, Mariana i&nbsp;Janine</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 2&nbsp;maja</b></td>
-            <td><b>Wspomnienie św.&nbsp;Atanazego</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
-                kapłańskie<br>
-            2. + Leszka Kondrata</td>
+            <td>1. W&nbsp;int. Moniki i&nbsp;Łukasza 4&nbsp;rocz.
+                ślubu<br>
+            2. + Janinę, Czesława Klimontów, Krystynę, Witolda
+                Szwedów, zm. z&nbsp;rodz. Klimontów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>1. + Andrzeja Wojnę – greg.<br>
-            2. Dzięk.-błag. o&nbsp;Boże błog., dla dzieci
-                i&nbsp;wnuczki</td>
+            2. O&nbsp;łaskę doświadczenia Bożej miłości dla męża
+                Piotra i&nbsp;ojca Tadeusza</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>SOBOTA, 3&nbsp;maja</b></td>
-            <td><b>Uroczystość NMP, Królowej Polski</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">7.30</td>
-            <td>+ Andrzeja Wojnę – greg.</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">9.30</td>
-            <td>+ Lucjana Sałka – rocz. śm.</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">11.15</td>
-            <td>Za&nbsp;parafian</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. LM<br>
-            2. + Mieczysława i&nbsp;Zofię Kucwajów</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 4&nbsp;maja</b></td>
+            <td><b>NIEDZIELA, 11&nbsp;maja</b></td>
             <td><b>
-                <span style="color: red">III&nbsp;Niedziela
+                <span style="color: red">IV&nbsp;Niedziela
                 Wielkanocna</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Ks. Bpa Piotra</td>
+            <td>+ Andrzeja Wojnę – greg.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Zofię i&nbsp;Władysława Chrząstowskich</td>
+            <td>+ Eugeniusza 7&nbsp;rocz. śm., Teresę i&nbsp;zm.
+                z&nbsp;rodz. Paciochów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>KZR – Róża Różańcowa z&nbsp;Milczan</td>
+            <td>+ Eleonorę, Jana Typków, zm. z&nbsp;rodz. Żórawskich
+                i&nbsp;Typków</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>Stanisławę Stefańską – 90&nbsp;rocz. urodzin –
-            o&nbsp;Boże błog., potrzebne łaski, opiekę MB</td>
+            <td>+ Stanisława, Zofię Stępniów, Marię Czech</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Andrzeja Wojnę – greg.</td>
+            <td>Za&nbsp;parafian</td>
         </tr>
     </tbody>
 </table>
