bec0afba93d82129f7cffcb7196699d6ea1daa42
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-06-01 21:09:15 +0100

    2025-06-01

--- archiwum14.php
+++ archiwum14.php
@@ -19,7 +19,156 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2025 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 18.&nbsp;maja 2025&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 1.&nbsp;czerwca 2025&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    26.05. – 1.06.2025&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 26&nbsp;maja</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Filipa Neri</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Henryka Pietruszkę – greg.<br>
+                                    2. O&nbsp;Boże błog., dla księdza Damiana Dziuby
+                                        w&nbsp;rocz. święceń</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Klarę, Jana, Ryszarda Chrząstowskich, Jana
+                                        Makowskiego<br>
+                                    2. + Anielę, Romualda, Amelię, Aleksandra Kapisów</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 27&nbsp;maja</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Stefana Żaka<br>
+                                    2. + Władysława Cieplińskiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Tadeusza Krupę<br>
+                                    2. + Henryka Pietruszkę – greg.</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 28&nbsp;maja</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Teodozję, Zdzisława Dudków, zm. z&nbsp;rodz.,
+                                        dusze w&nbsp;czyśścu cierpiące; Stanisławę
+                                        i&nbsp;Mariana Mądrych<br>
+                                    2. + Stefana Żaka</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Agnieszkę Pietrusińską 2&nbsp;rocz. śm.<br>
+                                    2. + Henryka Pietruszkę – greg.</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 29&nbsp;maja</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Urszuli Leduchowskiej</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Henryka Pietruszkę – greg.<br>
+                                    2. + Stefana Żaka</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Genowefę i&nbsp;Stanisława Rusaków<br>
+                                    2. + Ryszarda Kubickiego 17&nbsp;rocz. śm., Stanisława
+                                        Niewiadomskiego</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 30&nbsp;maja</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
+                                        Michaliny 26&nbsp;rocz. urodzin<br>
+                                    2. + Stefana Żaka</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. Adama, Stefanię Osuchów, Stanisława, Katarzynę,
+                                        Sławomira Rykowskich<br>
+                                    2. + Henryka Pietruszkę – greg.</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 31&nbsp;maja</b></td>
+                                    <td><b>Święto Nawiedzenia NMP</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Henryka Pietruszkę – greg.<br>
+                                    2. O&nbsp;Boże błog. dla kapłanów posługujących
+                                        w&nbsp;naszej parafii</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jana, Stanisławę, Witolda, Mariana, Stanisława,
+                                        Krystynę, Mariana i&nbsp;Janinę<br>
+                                    2. + Zofię, Stanisława, Wacława, Lucjana Wodzów</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 1&nbsp;czerwca</b></td>
+                                    <td><b>
+                                        <span style="color: red">VI&nbsp;Niedziela Wielkanocna
+                                        – Uroczystość Wniebowstąpienia Pańskiego</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Zofię, Wincentego, Henryka Wyrzykowskich, Bogusława
+                                        Stępińskiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Wiesławę, Weronikę, Zygmunta Szpatowiczów, Janine,
+                                        Kazimierza Malarzów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>K.Ż.R. – Jadwiga Mojduszko</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>Rocznica Pierwszej Komunii Świętej</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Henryka Pietruszkę – greg.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,67 +1,52 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            26.05. – 1.06.2025&nbsp;r.
+            2.06. – 8.06.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 26&nbsp;maja</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Filipa Neri</b></td>
+            <td><b>PONIEDZIAŁEK, 2&nbsp;czerwca</b></td>
+            <td><b>Wspomnienie Bł.&nbsp;Męczenników Sadoka
+            i&nbsp;Towarzyszy</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Zbigniewa Kamińskiego; Ludwikę i&nbsp;Jana
+                Kamińskich<br>
+            2. + Janinę Ziębę</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Tomasza Szyprowskiego<br>
+            2. + Henryka Pietruszkę – greg.</td>
+        </tr>
+
+
+        <tr class="actualnosci_day">
+            <td><b>WTOREK, 3&nbsp;czerwca</b></td>
+            <td><b>Wspomnienie Świętych Męczenników Karola Lwangi
+            i&nbsp;Towarzyszy</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>1. + Henryka Pietruszkę – greg.<br>
-            2. O&nbsp;Boże błog., dla księdza Damiana Dziuby
-                w&nbsp;rocz. święceń</td>
+            2. + Leszka Kondrata</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Klarę, Jana, Ryszarda Chrząstowskich, Jana
-                Makowskiego<br>
-            2. + Anielę, Romualda, Amelię, Aleksandra Kapisów</td>
+            <td>1. + Ludwikę Szczębarę, zm. z&nbsp;rodz. Szczębarów,
+                Golców, Królów, Ostrowskich i&nbsp;dusze
+                w&nbsp;czyśćcu cierpiące<br>
+            2. + Tadeusza Krupę</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 27&nbsp;maja</b></td>
+            <td><b>ŚRODA, 4&nbsp;czerwca</b></td>
             <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Stefana Żaka<br>
-            2. + Władysława Cieplińskiego</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Tadeusza Krupę<br>
-            2. + Henryka Pietruszkę – greg.</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>ŚRODA, 28&nbsp;maja</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Teodozję, Zdzisława Dudków, zm. z&nbsp;rodz.,
-                dusze w&nbsp;czyśścu cierpiące; Stanisławę
-                i&nbsp;Mariana Mądrych<br>
-            2. + Stefana Żaka</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Agnieszkę Pietrusińską 2&nbsp;rocz. śm.<br>
-            2. + Henryka Pietruszkę – greg.</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 29&nbsp;maja</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Urszuli Leduchowskiej</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
@@ -70,72 +55,91 @@
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Genowefę i&nbsp;Stanisława Rusaków<br>
-            2. + Ryszarda Kubickiego 17&nbsp;rocz. śm., Stanisława
-                Niewiadomskiego</td>
-        </tr>&nbsp;
+            <td>1. + Jana, Andrzeja Popków, Zofię, Janusza, Wiesława,
+                Jerzego Potrzebowskich, Annę i&nbsp;Michała
+                Sajórów<br>
+            2. + Wacława Czerwca</td>
+        </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 30&nbsp;maja</b></td>
-            <td><b></b></td>
+            <td><b>CZWARTEK, 5&nbsp;czerwca</b></td>
+            <td><b>Wspomnienie Św.&nbsp;Bonifacego - I&nbsp;Czwartek
+            miesiąca</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
-                Michaliny 26&nbsp;rocz. urodzin<br>
-            2. + Stefana Żaka</td>
+            <td>1. + Henryka Pietruszkę – greg.<br>
+            2. + Zofię i&nbsp;zm. z&nbsp;rodz. Żubrynowiczów,
+                Gradzońskich, Chałupczaków</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. Adama, Stefanię Osuchów, Stanisława, Katarzynę,
-                Sławomira Rykowskich<br>
+            <td>1. + Aleksandrę Ciamaga i&nbsp;zm. z&nbsp;rodz.<br>
+            2. + Genowefę 4&nbsp;rocz. śm., Jana Potrząsajów</td>
+        </tr>
+
+
+        <tr class="actualnosci_day">
+            <td><b>PIĄTEK, 6&nbsp;czerwca</b></td>
+            <td><b>I&nbsp;Piątek miesiąca</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Henryka Pietruszkę – greg.<br>
+            2. + Janinę Ziębę</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. O&nbsp;Boże błog., potrzebne łaski, opiekę MB
+                dla Hani 17&nbsp;rocz. ur.<br>
+            2. + Halinę i&nbsp;Kazimierza Masztaleruków</td>
+        </tr>
+
+
+        <tr class="actualnosci_day">
+            <td><b>SOBOTA, 7&nbsp;czerwca</b></td>
+            <td><b>I&nbsp;Sobota miesiąca</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                kapłańskie<br>
+            2. + Mieczysława, Helenę, Jana Wnuków, Katarzynę,
+                Stanisława Wyrzykowskich, Tadeusza Judę, Piotra
+                Bogdańskiego</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. LM<br>
             2. + Henryka Pietruszkę – greg.</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>SOBOTA, 31&nbsp;maja</b></td>
-            <td><b>Święto Nawiedzenia NMP</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Henryka Pietruszkę – greg.<br>
-            2. O&nbsp;Boże błog. dla kapłanów posługujących
-                w&nbsp;naszej parafii</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Jana, Stanisławę, Witolda, Mariana, Stanisława,
-                Krystynę, Mariana i&nbsp;Janinę<br>
-            2. + Zofię, Stanisława, Wacława, Lucjana Wodzów</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 1&nbsp;czerwca</b></td>
+            <td><b>NIEDZIELA, 8&nbsp;czerwca</b></td>
             <td><b>
-                <span style="color: red">VI&nbsp;Niedziela Wielkanocna
-                – Uroczystość Wniebowstąpienia Pańskiego</span>
+                <span style="color: red">Uroczystość Zesłania Ducha
+                Świętego</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Zofię, Wincentego, Henryka Wyrzykowskich, Bogusława
-                Stępińskiego</td>
+            <td>Dzięczynna z&nbsp;racji 50&nbsp;rocz. ślubu Bogusławy
+                i&nbsp;Jana</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Wiesławę, Weronikę, Zygmunta Szpatowiczów, Janine,
-                Kazimierza Malarzów</td>
+            <td>+ Mariannę, Kazimierza Kieszkowskich, zm. z&nbsp;rodz.
+                Kieszkowskich i&nbsp;Nizielskich</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>K.Ż.R. – Jadwiga Mojduszko</td>
+            <td>+ Mariana rocz. śm., Helenę Przyłuckich</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>Rocznica Pierwszej Komunii Świętej</td>
+            <td>O&nbsp;Boże błog. dla Helenki, dzięk.-błagalna</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
