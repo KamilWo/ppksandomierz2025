f84a67e85fa58433b15b9b90be2fffa49b10ba65
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-06-09 00:53:31 +0100

    2025-06-09

--- archiwum14.php
+++ archiwum14.php
@@ -19,7 +19,160 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2025 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 1.&nbsp;czerwca 2025&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 8.&nbsp;czerwca 2025&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    2.06. – 8.06.2025&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 2&nbsp;czerwca</b></td>
+                                    <td><b>Wspomnienie Bł.&nbsp;Męczenników Sadoka
+                                    i&nbsp;Towarzyszy</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Zbigniewa Kamińskiego; Ludwikę i&nbsp;Jana
+                                        Kamińskich<br>
+                                    2. + Janinę Ziębę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Tomasza Szyprowskiego<br>
+                                    2. + Henryka Pietruszkę – greg.</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 3&nbsp;czerwca</b></td>
+                                    <td><b>Wspomnienie Świętych Męczenników Karola Lwangi
+                                    i&nbsp;Towarzyszy</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Henryka Pietruszkę – greg.<br>
+                                    2. + Leszka Kondrata</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Ludwikę Szczębarę, zm. z&nbsp;rodz. Szczębarów,
+                                        Golców, Królów, Ostrowskich i&nbsp;dusze
+                                        w&nbsp;czyśćcu cierpiące<br>
+                                    2. + Tadeusza Krupę</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 4&nbsp;czerwca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Henryka Pietruszkę – greg.<br>
+                                    2. + Stefana Żaka</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Jana, Andrzeja Popków, Zofię, Janusza, Wiesława,
+                                        Jerzego Potrzebowskich, Annę i&nbsp;Michała
+                                        Sajórów<br>
+                                    2. + Wacława Czerwca</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 5&nbsp;czerwca</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Bonifacego - I&nbsp;Czwartek
+                                    miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Henryka Pietruszkę – greg.<br>
+                                    2. + Zofię i&nbsp;zm. z&nbsp;rodz. Żubrynowiczów,
+                                        Gradzońskich, Chałupczaków</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Aleksandrę Ciamaga i&nbsp;zm. z&nbsp;rodz.<br>
+                                    2. + Genowefę 4&nbsp;rocz. śm., Jana Potrząsajów</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 6&nbsp;czerwca</b></td>
+                                    <td><b>I&nbsp;Piątek miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. + Henryka Pietruszkę – greg.<br>
+                                    2. + Janinę Ziębę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. O&nbsp;Boże błog., potrzebne łaski, opiekę MB
+                                        dla Hani 17&nbsp;rocz. ur.<br>
+                                    2. + Halinę i&nbsp;Kazimierza Masztaleruków</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 7&nbsp;czerwca</b></td>
+                                    <td><b>I&nbsp;Sobota miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                                        kapłańskie<br>
+                                    2. + Mieczysława, Helenę, Jana Wnuków, Katarzynę,
+                                        Stanisława Wyrzykowskich, Tadeusza Judę, Piotra
+                                        Bogdańskiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. LM<br>
+                                    2. + Henryka Pietruszkę – greg.</td>
+                                </tr>
+
+
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 8&nbsp;czerwca</b></td>
+                                    <td><b>
+                                        <span style="color: red">Uroczystość Zesłania Ducha
+                                        Świętego</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>Dzięczynna z&nbsp;racji 50&nbsp;rocz. ślubu Bogusławy
+                                        i&nbsp;Jana</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Mariannę, Kazimierza Kieszkowskich, zm. z&nbsp;rodz.
+                                        Kieszkowskich i&nbsp;Nizielskich</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ Mariana rocz. śm., Helenę Przyłuckich</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>O&nbsp;Boże błog. dla Helenki, dzięk.-błagalna</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Henryka Pietruszkę – greg.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,149 +1,150 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            2.06. – 8.06.2025&nbsp;r.
+            9.06. – 15.06.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 2&nbsp;czerwca</b></td>
-            <td><b>Wspomnienie Bł.&nbsp;Męczenników Sadoka
-            i&nbsp;Towarzyszy</b></td>
+            <td><b>PONIEDZIAŁEK, 9&nbsp;czerwca</b></td>
+            <td><b>Święto NMP, Matki Kościoła</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Zbigniewa Kamińskiego; Ludwikę i&nbsp;Jana
-                Kamińskich<br>
-            2. + Janinę Ziębę</td>
+            <td>1. O&nbsp;potrzebne łaski dla dzieci
+                i&nbsp;chrześniaków<br>
+            2. + Pawła Piątka</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Tomasza Szyprowskiego<br>
-            2. + Henryka Pietruszkę – greg.</td>
+            <td>1. + Helenę i&nbsp;Ludwika Wabiów w&nbsp;rocz. śm.,
+                zm. z&nbsp;rodz.<br>
+            2. + Henryka Pietruszkę – greg. </td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 3&nbsp;czerwca</b></td>
-            <td><b>Wspomnienie Świętych Męczenników Karola Lwangi
-            i&nbsp;Towarzyszy</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Henryka Pietruszkę – greg.<br>
-            2. + Leszka Kondrata</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Ludwikę Szczębarę, zm. z&nbsp;rodz. Szczębarów,
-                Golców, Królów, Ostrowskich i&nbsp;dusze
-                w&nbsp;czyśćcu cierpiące<br>
-            2. + Tadeusza Krupę</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>ŚRODA, 4&nbsp;czerwca</b></td>
+            <td><b>WTOREK, 10&nbsp;czerwca</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>1. + Henryka Pietruszkę – greg.<br>
-            2. + Stefana Żaka</td>
+            2. + Pawła Piątka</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Jana, Andrzeja Popków, Zofię, Janusza, Wiesława,
-                Jerzego Potrzebowskich, Annę i&nbsp;Michała
-                Sajórów<br>
-            2. + Wacława Czerwca</td>
+            <td>1. KWC<br>
+            2. + O&nbsp;łaskę doświadczenia Bożej miłości dla męża
+                Piotra i&nbsp;ojca Tadeusza</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 5&nbsp;czerwca</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Bonifacego - I&nbsp;Czwartek
-            miesiąca</b></td>
+            <td><b>ŚRODA, 11&nbsp;czerwca</b></td>
+            <td><b>Wspomnienie Św.&nbsp;Barnaby</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. + Henryka Pietruszkę – greg.<br>
-            2. + Zofię i&nbsp;zm. z&nbsp;rodz. Żubrynowiczów,
-                Gradzońskich, Chałupczaków</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>1. + Aleksandrę Ciamaga i&nbsp;zm. z&nbsp;rodz.<br>
-            2. + Genowefę 4&nbsp;rocz. śm., Jana Potrząsajów</td>
-        </tr>
-
-
-        <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 6&nbsp;czerwca</b></td>
-            <td><b>I&nbsp;Piątek miesiąca</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>1. + Henryka Pietruszkę – greg.<br>
+            <td>1. O&nbsp;łaskę dobrej śmierci<br>
             2. + Janinę Ziębę</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. O&nbsp;Boże błog., potrzebne łaski, opiekę MB
-                dla Hani 17&nbsp;rocz. ur.<br>
-            2. + Halinę i&nbsp;Kazimierza Masztaleruków</td>
+            <td>1. O&nbsp;Boże błog., potrzebne łaski, opiekę MB dla
+                Izabeli i&nbsp;Arnolda – 20&nbsp;rocz. ślubu
+                i&nbsp;dzieci Rozalii, i&nbsp;Wojciecha<br>
+            2. + Henryka Pietruszkę – greg. </td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>SOBOTA, 7&nbsp;czerwca</b></td>
-            <td><b>I&nbsp;Sobota miesiąca</b></td>
+            <td><b>CZWARTEK, 12&nbsp;czerwca</b></td>
+            <td><b>Święto Jezusa Chrystusa, Najwyższego
+            i&nbsp;Wiecznego Kapłana</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>1. O&nbsp;świętość kapłanów i&nbsp;nowe powołania
-                kapłańskie<br>
-            2. + Mieczysława, Helenę, Jana Wnuków, Katarzynę,
-                Stanisława Wyrzykowskich, Tadeusza Judę, Piotra
-                Bogdańskiego</td>
+            <td>1. + Henryka Pietruszkę – greg.<br>
+            2. + Gustawa Sadłochę 14&nbsp;rocz. śm., Władysława,
+                Józefę Sajdów, Stefana i&nbsp;Annę Sadłochów,
+                i&nbsp;zm. z&nbsp;rodz.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. LM<br>
-            2. + Henryka Pietruszkę – greg.</td>
+            <td>1. + Jana Ozdobę – rocz. śm.<br>
+            2. + Marka Stefaniaka</td>
         </tr>
 
 
         <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 8&nbsp;czerwca</b></td>
+            <td><b>PIĄTEK, 13&nbsp;czerwca</b></td>
+            <td><b>Wspomnienie św.&nbsp;Antoniego z&nbsp;Padwy</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Henryka Pietruszkę – greg.<br>
+            2. + Anielę i&nbsp;Antoniego Gospodarczyków</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Bogusława Sarzyńskiego, Annę, Janusza, Stanisława
+                Roźmiejów, Zofię, Mikołaja, Eugeniusza Kapustów<br>
+            2. + Marię, Władysława, Czesława Barańskich, Andrzeja
+                Krawczyka</td>
+        </tr>
+
+
+        <tr class="actualnosci_day">
+            <td><b>SOBOTA, 14&nbsp;czerwca</b></td>
+            <td><b>Wspomnienie bł.&nbsp;Michała Kozala</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>1. + Janinę Ziębę<br>
+            2. O&nbsp;Boże błog., opiekę MB, potrzebne łaski dla
+                Władysławy w&nbsp;82&nbsp;rocz. urodzin</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>1. + Barbarę, Stanisława Stańków, Andrzeja
+                Stoinskiego<br>
+            2. + Hentyka Pietruszkę – greg.</td>
+        </tr>
+
+
+        <tr class="actualnosci_day">
+            <td><b>NIEDZIELA, 15&nbsp;czerwca</b></td>
             <td><b>
-                <span style="color: red">Uroczystość Zesłania Ducha
-                Świętego</span>
+                <span style="color: red">Uroczystość Najświętszej
+                Trójcy</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>Dzięczynna z&nbsp;racji 50&nbsp;rocz. ślubu Bogusławy
-                i&nbsp;Jana</td>
+            <td>+ Hentyka Pietruszkę – greg.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Mariannę, Kazimierza Kieszkowskich, zm. z&nbsp;rodz.
-                Kieszkowskich i&nbsp;Nizielskich</td>
+            <td>Dziękczynna z&nbsp;racji 50&nbsp;rocz. ślubu Krystyny
+                i&nbsp;Henryka Słowików<br>
+            Za&nbsp;parafian</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>+ Mariana rocz. śm., Helenę Przyłuckich</td>
+            <td>DK</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>O&nbsp;Boże błog. dla Helenki, dzięk.-błagalna</td>
+            <td>+ Zofię, Wawrzyńca Kwietniów, zm. z&nbsp;rodz.
+                Kwietniów i&nbsp;Kondeuszów, Jana, Tadeusza Niezgodów
+                i&nbsp;zm. z&nbsp;rodz. Niezgodów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Henryka Pietruszkę – greg.</td>
+            <td>+ Andrzeja Wojnę i&nbsp;zm. z&nbsp;rodz.</td>
         </tr>
     </tbody>
 </table>
