54ab4953e1485e97c50a6254be3f8d5499aff8bc
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-07-13 12:37:08 +0100

    2025-07-13

--- archiwum14.php
+++ archiwum14.php
@@ -19,7 +19,141 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2025 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 29.&nbsp;czerwca 2025&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 6.&nbsp;lipca 2025&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    30.06. – 6.07.2025&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="aktualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 30&nbsp;czerwca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Pawła Piątka</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Mieczysława, Józefa, Mariannę i&nbsp;zm.
+                                    z&nbsp;rodz.</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>WTOREK, 1&nbsp;lipca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Halinę Hagdej – 4&nbsp;rocz. śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>O&nbsp;Boże błog., dary Ducha Św., opiekę MB dla
+                                    Józefa Pająka w&nbsp;70&nbsp;rocz. ur.</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>ŚRODA, 2&nbsp;lipca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Janinę Ziębę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Mariana i&nbsp;Weronikę Trzosów</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>CZWARTEK, 3&nbsp;lipca</b></td>
+                                    <td><b>Święto Św.&nbsp;Tomasza Apostoła – I&nbsp;Czwartek
+                                    miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                                        kapłańskie</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Jana, Stanisławę, Witolda, Mariana, Stanisława,
+                                    Krystynę, Mariana i&nbsp;Janinę</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>PIĄTEK, 4&nbsp;lipca</b></td>
+                                    <td><b>I&nbsp;Piątek miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>O&nbsp;potrzebne łaski dla dzieci
+                                    i&nbsp;chrześniaków</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>O&nbsp;Boże błog. dla Emilii w&nbsp;18&nbsp;rocz.
+                                    ur.</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>SOBOTA, 5&nbsp;lipca</b></td>
+                                    <td><b>I&nbsp;Sobota miesiąca</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>O&nbsp;łaskę dobrej śmierci</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>LM</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>NIEDZIELA, 6&nbsp;lipca</b></td>
+                                    <td><b>
+                                        <span style="color: red">XIV&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Józefę Marzec rocz. śm., Jana, Marię, Ryszarda
+                                    Pietruszków, zm. z&nbsp;rodz. Klimontów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Henryka, Leona Sochów, Stefana Stępnia</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>K.Ż.R. – zelator Halina Kawiorska</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Zofię rocz. śm., Henryka, Krystiana, Mieczysława
+                                    Kucwajów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,125 +1,124 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            30.06. – 6.07.2025&nbsp;r.
+            14.06. – 20.07.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="aktualnosci_day">
-            <td><b>PONIEDZIAŁEK, 30&nbsp;czerwca</b></td>
+            <td><b>PONIEDZIAŁEK, 14&nbsp;lipca</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Pawła Piątka</td>
+            <td>+ Teodozję Zięba</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Mieczysława, Józefa, Mariannę i&nbsp;zm.
-            z&nbsp;rodz.</td>
+            <td>+ Janusza Tworowskiego 3&nbsp;rocz. śm.</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>WTOREK, 1&nbsp;lipca</b></td>
+            <td><b>WTOREK, 15&nbsp;lipca</b></td>
+            <td><b>Wspomnienie Św.&nbsp;Bonawentury</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>+ Janinę Zięba</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>+ Stanisława, Reginę, Marka Zakrzewskich, zm.
+            z&nbsp;rodz. Świątańskich</td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>ŚRODA, 16&nbsp;lipca</b></td>
+            <td><b>Wspomnienie NMP z&nbsp;Góry Karmel</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>+ Zofię, Wincentego Kołodziejów, Juliannę, Walentego
+                Bulirów, Irenę i&nbsp;Tadeusza Kołaczów, Marię, Kazimierza
+                Włochów, Stanisława, Stanisławę Wosiów</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>+ Roberta Drozda rocz. śm.</td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>CZWARTEK, 17&nbsp;lipca</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Halinę Hagdej – 4&nbsp;rocz. śm.</td>
+            <td>+ Teodozję Zięba</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>O&nbsp;Boże błog., dary Ducha Św., opiekę MB dla
-            Józefa Pająka w&nbsp;70&nbsp;rocz. ur.</td>
+            <td>+ Kazimierza, Mariannę, Henryka Lipców, Mariannę
+                i&nbsp;Walentego Grzesików</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>ŚRODA, 2&nbsp;lipca</b></td>
+            <td><b>PIĄTEK, 18&nbsp;lipca</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Janinę Ziębę</td>
+            <td>+ Emilię i&nbsp;Edwarda Paluchów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Mariana i&nbsp;Weronikę Trzosów</td>
+            <td>+ Wincentego i&nbsp;Zofię Wiatrowskich</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>CZWARTEK, 3&nbsp;lipca</b></td>
-            <td><b>Święto Św.&nbsp;Tomasza Apostoła – I&nbsp;Czwartek
-            miesiąca</b></td>
+            <td><b>SOBOTA, 19&nbsp;lipca</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>O świętość kapłanów i&nbsp;nowe powołania kapłańskie</td>
+            <td>+ Mieczysława i&nbsp;Barbarę Bryłów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Jana, Stanisławę, Witolda, Mariana, Stanisława,
-            Krystynę, Mariana i&nbsp;Janinę</td>
+            <td>+ Edmunda, Helenę, Jana Szafrańskich, Teofilę,
+                Walentego Adamców</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>PIĄTEK, 4&nbsp;lipca</b></td>
-            <td><b>I Piątek miesiąca</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>O&nbsp;potrzebne łaski dla dzieci
-            i&nbsp;chrześniaków</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>O&nbsp;Boże błog. dla Emilii w&nbsp;18&nbsp;rocz.
-            ur.</td>
-        </tr>
-
-
-        <tr class="aktualnosci_day">
-            <td><b>SOBOTA, 5&nbsp;lipca</b></td>
-            <td><b>I&nbsp;Sobota miesiąca</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>O&nbsp;łaskę dobrej śmierci</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>LM</td>
-        </tr>
-
-
-        <tr class="aktualnosci_day">
-            <td><b>NIEDZIELA, 6&nbsp;lipca</b></td>
+            <td><b>NIEDZIELA, 20&nbsp;lipca</b></td>
             <td><b>
-                <span style="color: red">XIV&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">XVI&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Józefę Marzec rocz. śm., Jana, Marię, Ryszarda
-            Pietruszków, zm. z&nbsp;rodz. Klimontów</td>
+            <td>Dziękczynna z&nbsp;racji 50&nbsp;rocz. ślubu Anny
+                i&nbsp;Zbigniewa</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Henryka, Leona Sochów, Stefana Stępnia</td>
+            <td>+ Teresę Piorun – 3&nbsp;rocz. śm.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>K.Ż.R. – zelator Halina Kawiorska</td>
+            <td>DK</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>+ Zofię rocz. śm., Henryka, Krystiana, Mieczysława
-            Kucwajów</td>
+            <td>+ Otylię, Juliusza Nagórnych, zm. z&nbsp;rodz.
+                Grzesiaków i&nbsp;Nagórnych</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
