3f1ca825826e50448b964d6ee6236567222adf0a
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-07-26 13:03:12 +0100

    2025-07-27

--- archiwum14.php
+++ archiwum14.php
@@ -19,12 +19,148 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2025 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 20.&nbsp;lipca 2025&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 27.&nbsp;lipca 2025&nbsp;r.</h2>
 
                         <div class="card">
                             <div class="card-header">
                                 <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-                                    14.06. – 20.07.2025&nbsp;r.
+                                    21.07. – 27.07.2025&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="aktualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 21&nbsp;lipca</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Zofię i&nbsp;Czesława Chlebnych w&nbsp;rocz.
+                                    śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Marianę, Jakuba, Mieczysława Pijewskich</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>WTOREK, 22&nbsp;lipca</b></td>
+                                    <td><b>Święto Św.&nbsp;Marii Magdaleny</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Teodozję Zięba</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>1. + Annę Błażuk – rocz. śm.<br>
+                                    2. O&nbsp;Boże błog., potrzebne łaski, dary Ducha Św.,
+                                        opiekę MB dla Haliny i&nbsp;Kazimierza, i&nbsp;ich
+                                        rodziny z&nbsp;okazji 44&nbsp;rocz. Sakramentu
+                                        małżeństwa</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>ŚRODA, 23&nbsp;lipca</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Brygidy</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Andrzeja Karelusa</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Mariannę i&nbsp;Tadeusza Stefaniaków</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>CZWARTEK, 24&nbsp;lipca</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Kingi</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Otolię, Władysława, Tomasza, Eugeniusza</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Włodzimierza, Karolinę, Stanisława Bartosików,
+                                        Mariannę i&nbsp;Romana Patynów</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>PIĄTEK, 25&nbsp;lipca</b></td>
+                                    <td><b>Święto Św.&nbsp;Jakuba</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Janinę Ziębę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Krzysztofa, Stefanię, Józefa Łukawskich, Stanisława
+                                        i&nbsp;Leokadię Majcherów</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>SOBOTA, 26&nbsp;lipca</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Joachima i&nbsp;Anny,
+                                    Rodziców NMP</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>W&nbsp;int. Anny – imieninowa o&nbsp;Boże błog.,
+                                        potrzebne łaski</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>Dzięk.-błag. o&nbsp;Boże błog. dla Dawida</td>
+                                </tr>
+
+
+                                <tr class="aktualnosci_day">
+                                    <td><b>NIEDZIELA, 27&nbsp;lipca</b></td>
+                                    <td><b>
+                                        <span style="color: red">XVII&nbsp;Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Janinę, Stanisława, Zdzisława Kulów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Jana, Bronisławę, Franciszka Zająców, Mariannę Zając</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ Bogusława Sarzyńskiego, Annę, Janusza, Stanisława
+                                        Roźmiejów, Zofię, Mikołaja, Eugeniusza Kapustów</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Józefę, Stanisława Wolaków, Mariannę i&nbsp;Adama
+                                        Bąków, zm. z&nbsp;rodz.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    14.07. – 20.07.2025&nbsp;r.
                                 </h3>
                             </div>
                             <div class="col">
--- last_intentions.php
+++ last_intentions.php
@@ -1,132 +1,132 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            14.06. – 27.07.2025&nbsp;r.
+            28.07. – 03.08.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="aktualnosci_day">
-            <td><b>PONIEDZIAŁEK, 21&nbsp;lipca</b></td>
+            <td><b>PONIEDZIAŁEK, 28&nbsp;lipca</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Zofię i&nbsp;Czesława Chlebnych w&nbsp;rocz.
-            śm.</td>
+            <td>+ Piotra Kozłowskiego, Alicję i&nbsp;Wacława
+                Chaniewskich, Mariannę i&nbsp;Stanisława Kozłowskich,
+                Mariannę i&nbsp;Jana Samela</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Marianę, Jakuba, Mieczysława Pijewskich</td>
+            <td>+ Jana Kusala</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>WTOREK, 22&nbsp;lipca</b></td>
-            <td><b>Święto Św.&nbsp;Marii Magdaleny</b></td>
+            <td><b>WTOREK, 29&nbsp;lipca</b></td>
+            <td><b>Wspomienie Św.&nbsp;Marty, Marii
+            i&nbsp;Łazarza</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Teodozję Zięba</td>
+            <td>+ Janinę Zięba</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>1. + Annę Błażuk – rocz. śm.<br>
-            2. O&nbsp;Boże błog., potrzebne łaski, dary Ducha Św.,
-                opiekę MB dla Haliny i&nbsp;Kazimierza, i&nbsp;ich
-                rodziny z&nbsp;okazji 44&nbsp;rocz. Sakramentu
-                małżeństwa</td>
+            <td>+ Mieczysława Mrozińskiego rocz. śm., Teresę
+                i&nbsp;Stanisława Libickich</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>ŚRODA, 23&nbsp;lipca</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Brygidy</b></td>
+            <td><b>ŚRODA, 30&nbsp;lipca</b></td>
+            <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Andrzeja Karelusa</td>
+            <td>+ Barbarę Pomykałę</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Mariannę i Tadeusza Stefaniaków</td>
+            <td>+ Stefana rocz. śm., Genowefę, Jarosława, Annę,
+                Józefę Rurków, Paulinę, Mieczysława Romusów, Janinę,
+                Stefana Plachów, Kazimierę Pietras, Marię Potocką,
+                Józefę i&nbsp;Romana Siudaków</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>CZWARTEK, 24&nbsp;lipca</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Kingi</b></td>
+            <td><b>CZWARTEK, 31&nbsp;lipca</b></td>
+            <td><b>Wspomnienie Św.&nbsp;Ignacego z&nbsp;Loyoli</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Otolię, Władysława, Tomasza, Eugeniusza</td>
+            <td>+ Teodozję Ziębę</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Włodzimierza, Karolinę, Stanisława Bartosików,
-                Mariannę i&nbsp;Romana Patynów</td>
+            <td>+ Małgorzatę Walczynę 8&nbsp;rocz. śm.</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>PIĄTEK, 25&nbsp;lipca</b></td>
-            <td><b>Święto Św.&nbsp;Jakuba</b></td>
+            <td><b>PIĄTEK, 1&nbsp;sierpnia</b></td>
+            <td><b>Wspomnienie Św.&nbsp;Alfonsa Liguoriego –
+            I&nbsp;Piątek miesiąca</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Janinę Ziębę</td>
+            <td>O&nbsp;świętość kapłanów i&nbsp;nowe powołania
+                kapłańskie</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Krzysztofa, Stefanię, Józefa Łukawskich, Stanisława
-                i&nbsp;Leokadię Majcherów</td>
+            <td>+ Mariannę i&nbsp;Eugeniusza Jedlińskich</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>SOBOTA, 26&nbsp;lipca</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Joachima i&nbsp;Anny,
-            Rodziców NMP</b></td>
+            <td><b>SOBOTA, 2&nbsp;sierpnia</b></td>
+            <td><b>I&nbsp;Sobota miesiąca</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>W&nbsp;int. Anny – imieninowa o&nbsp;Boże błog.,
-                potrzebne łaski</td>
+            <td>O&nbsp;potrzebne łaski dla dzieci i&nbsp;chrześniaków</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>Dzięk.-błag. o&nbsp;Boże błog. dla Dawida</td>
+            <td>LM</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>NIEDZIELA, 27&nbsp;lipca</b></td>
+            <td><b>NIEDZIELA, 3&nbsp;sierpnia</b></td>
             <td><b>
-                <span style="color: red">XVII&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">XVIII&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Janinę, Stanisława, Zdzisława Kulów</td>
+            <td>Za&nbsp;parafian</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Jana, Bronisławę, Franciszka Zająców, Mariannę Zając</td>
+            <td>+ Katarzynę i&nbsp;Franciszka Sadowskich</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>+ Bogusława Sarzyńskiego, Annę, Janusza, Stanisława
-                Roźmiejów, Zofię, Mikołaja, Eugeniusza Kapustów</td>
+            <td>KZR Teresa Borkowska</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>Za parafian</td>
+            <td>+ Józefa, Stefana, Artura Steców, Janinę, Mariana
+                i&nbsp;Zbigniewa Błajszczaków</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Józefę, Stanisława Wolaków, Mariannę i&nbsp;Adama
-                Bąków, zm. z&nbsp;rodz.</td>
+            <td>+ Jana, Stanisławę, Witolda, Mariana, Stanisława,
+                Krystynę, Mariana i&nbsp;Janinę</td>
         </tr>
     </tbody>
 </table>
