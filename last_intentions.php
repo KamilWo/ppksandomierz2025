6daaecb45a9e39aaa0ff9552ba87c849eb1a2280
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-08-20 07:28:41 +0100

    2025-08-17

--- last_intentions.php
+++ last_intentions.php
@@ -1,64 +1,29 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            28.07. – 03.08.2025&nbsp;r.
+            18.08. – 24.08.2025&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="aktualnosci_day">
-            <td><b>PONIEDZIAŁEK, 28&nbsp;lipca</b></td>
+            <td><b>PONIEDZIAŁEK, 18&nbsp;sierpnia</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Piotra Kozłowskiego, Alicję i&nbsp;Wacława
-                Chaniewskich, Mariannę i&nbsp;Stanisława Kozłowskich,
-                Mariannę i&nbsp;Jana Samela</td>
+            <td>+ Zofię i&nbsp;Józefa Bulirów, Józefę Kowalczewską</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Jana Kusala</td>
+            <td>+ Stanisława Kazbę</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>WTOREK, 29&nbsp;lipca</b></td>
-            <td><b>Wspomienie Św.&nbsp;Marty, Marii
-            i&nbsp;Łazarza</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Janinę Zięba</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>+ Mieczysława Mrozińskiego rocz. śm., Teresę
-                i&nbsp;Stanisława Libickich</td>
-        </tr>
-
-
-        <tr class="aktualnosci_day">
-            <td><b>ŚRODA, 30&nbsp;lipca</b></td>
+            <td><b>WTOREK, 19&nbsp;sierpnia</b></td>
             <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Barbarę Pomykałę</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>+ Stefana rocz. śm., Genowefę, Jarosława, Annę,
-                Józefę Rurków, Paulinę, Mieczysława Romusów, Janinę,
-                Stefana Plachów, Kazimierę Pietras, Marię Potocką,
-                Józefę i&nbsp;Romana Siudaków</td>
-        </tr>
-
-
-        <tr class="aktualnosci_day">
-            <td><b>CZWARTEK, 31&nbsp;lipca</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Ignacego z&nbsp;Loyoli</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
@@ -66,67 +31,94 @@
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Małgorzatę Walczynę 8&nbsp;rocz. śm.</td>
+            <td>+ Jerzego Gawrońskiego, Stefanię i&nbsp;Stanisława
+            Kołodziejów</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>PIĄTEK, 1&nbsp;sierpnia</b></td>
-            <td><b>Wspomnienie Św.&nbsp;Alfonsa Liguoriego –
-            I&nbsp;Piątek miesiąca</b></td>
+            <td><b>ŚRODA, 20&nbsp;sierpnia</b></td>
+            <td><b>Wspomnienie św.&nbsp;Bernarda</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>O&nbsp;świętość kapłanów i&nbsp;nowe powołania
-                kapłańskie</td>
+            <td>+ Marię Szwagierczak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Mariannę i&nbsp;Eugeniusza Jedlińskich</td>
+            <td>+ Elżbietę Sapielak</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>SOBOTA, 2&nbsp;sierpnia</b></td>
-            <td><b>I&nbsp;Sobota miesiąca</b></td>
+            <td><b>CZWARTEK, 21&nbsp;sierpnia</b></td>
+            <td><b>Wspomnienie św.&nbsp;Piusa&nbsp;X</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>O&nbsp;potrzebne łaski dla dzieci i&nbsp;chrześniaków</td>
+            <td>+ Marię Szwagierczak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>LM</td>
+            <td>+ Henryka Pietruszkę</td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>NIEDZIELA, 3&nbsp;sierpnia</b></td>
+            <td><b>PIĄTEK, 22&nbsp;sierpnia</b></td>
+            <td><b>Wspomnienie NMP Królowej</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>+ Roberta Rubisia</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>+ Adama, Stefanię Osuchów, Stanisława, Katarzynę,
+            Sławomira Rykowkich</td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>SOBOTA, 23&nbsp;sierpnia</b></td>
+            <td><b></b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>+ Martę Łabęcką</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>O&nbsp;Boże błog., opiekę MB, potrzebne łaski dla
+            Michała i&nbsp;Gabriela</td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>NIEDZIELA, 24&nbsp;sierpnia</b></td>
             <td><b>
-                <span style="color: red">XVIII&nbsp;Niedziela Zwykła</span>
+                <span style="color: red">XXI&nbsp;Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>Za&nbsp;parafian</td>
+            <td>+ Jana, Stanisławę, Krystynę Przytułów</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>+ Katarzynę i&nbsp;Franciszka Sadowskich</td>
+            <td>+ Jana Mikulskiego</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>KZR Teresa Borkowska</td>
+            <td>+ Józefa Kicińskiego</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>+ Józefa, Stefana, Artura Steców, Janinę, Mariana
-                i&nbsp;Zbigniewa Błajszczaków</td>
+            <td>Za&nbsp;parafian</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Jana, Stanisławę, Witolda, Mariana, Stanisława,
-                Krystynę, Mariana i&nbsp;Janinę</td>
+            <td>+ Helenę i&nbsp;Jana Pazdro</td>
         </tr>
     </tbody>
 </table>
