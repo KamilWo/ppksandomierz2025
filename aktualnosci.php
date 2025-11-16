71266e046e39679111245dd9b900d8f3ded0937b
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-11-16 09:33:14 +0000

    2025-11-16

--- aktualnosci.php
+++ aktualnosci.php
@@ -1,6 +1,6 @@
 <h2 class="first-content-header"><i class="fa fa-newspaper"></i> Aktualności najnowsze</h2>
 <?php
-include 'last_post.php';
+include 'posts.php';
 ?>
 <h4 class="mt-2">Poprzednie wpisy przeniesiono do <a href="archiwum.php">archiwum</a>.</h4>
 <?php
